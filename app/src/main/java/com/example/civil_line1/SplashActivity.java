ba90eef8f55f3c097e238972e6383ccb9fd1137a
Origaska <lucio.hernandez.noguez@gmail.com>
2021-12-18 12:35:07 -0600

    Pirmer pantalla y splash

--- app/src/main/java/com/example/civil_line1/SplashActivity.java
+++ app/src/main/java/com/example/civil_line1/SplashActivity.java
@@ -2,13 +2,17 @@ package com.example.civil_line1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity2 extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
