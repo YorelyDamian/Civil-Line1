f517007f411ba1c25d284cbdc702cd97e517a106
Origaska <lucio.hernandez.noguez@gmail.com>
2021-12-18 14:51:19 -0600

    Agregando acciones hacia entradas de datos

--- app/src/main/java/com/example/civil_line1/MainActivity.java
+++ app/src/main/java/com/example/civil_line1/MainActivity.java
@@ -5,11 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.Switch;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton btnClose;
     private ImageButton btnHistory;
+    private Button btnContinue;
+    private RadioButton btnHorizontal;
+    private RadioButton btnSpiral;
+    private SwitchMaterial switchDirection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,26 @@ public class MainActivity extends AppCompatActivity {
                 createIntent(History.class);
             }
         });
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*direction true is left
+                * direction false is right
+                */
+
+                boolean direction = true;
+                if (switchDirection.isChecked()){
+                    /*Right*/
+                    direction = false;
+                }
+                if(btnSpiral.isChecked()){
+                    createIntent(SpiralValues.class);
+                }else if(btnHorizontal.isChecked()){
+                    createIntent(HorizontalValues.class);
+                }
+            }
+        });
     }
 
 
@@ -48,5 +77,9 @@ public class MainActivity extends AppCompatActivity {
     private void initElements(){
         btnClose = (ImageButton) findViewById(R.id.main_close);
         btnHistory = (ImageButton) findViewById(R.id.main_history);
+        btnContinue = (Button) findViewById(R.id.btn_Continuar);
+        btnHorizontal = (RadioButton) findViewById(R.id.radioHorizontal);
+        btnSpiral = (RadioButton) findViewById(R.id.radioEspiral);
+        switchDirection = (SwitchMaterial) findViewById(R.id.switchDirection);
     }
 }
--- app/src/main/java/com/example/civil_line1/SpiralValues.java
+++ app/src/main/java/com/example/civil_line1/SpiralValues.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity4 extends AppCompatActivity {
+public class SpiralValues extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main4);
+        setContentView(R.layout.spiral_layout);
     }
 }
