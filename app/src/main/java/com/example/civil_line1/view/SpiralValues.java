2f928f875455de186798a9dace525a894c5ac053
YorelyDamian <yorely1200@gmail.com>
2021-12-23 15:30:37 -0600

    Envio de cambios de Spiral

--- app/src/main/java/com/example/civil_line1/view/SpiralValues.java
+++ app/src/main/java/com/example/civil_line1/view/SpiralValues.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.text.TextWatcher;
 
 import com.example.civil_line1.R;
 
@@ -15,6 +18,13 @@ public class SpiralValues extends AppCompatActivity {
     private ImageButton btnSave;
     private ImageButton btnClear;
     private Button btnOperations;
+    private EditText AngTan;
+    private EditText PI;
+    private EditText VP;
+    private EditText GC;
+    private EditText Le;
+    private EditText DC;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +50,72 @@ public class SpiralValues extends AppCompatActivity {
         btnOperations.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createIntent(OperationsActivity.class);
+                String nombre = btnOperations.getText().toString();
+                if(nombre.toLowerCase().equals("calcular")){
+                    if(estanLlenos()){
+                        btnOperations.setText("Operaciones");
+                        //carga el evento de los datos
+                    }
+                }else{
+                    btnOperations.setText("Calcular");
+                    createIntent(OperationsActivity.class);
+                }
             }
         });
-
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AngTan.setText(null);
+                PI.setText(null);
+                VP.setText(null);
+                Le.setText(null);
+                GC.setText(null);
+                DC.setText(null);
+            }
+        });
+        cargarEvento(AngTan);
+        cargarEvento(PI);
+        cargarEvento(VP);
+        cargarEvento(Le);
+        cargarEvento(GC);
+        cargarEvento(DC);
     }
 
+    private void cargarEvento(EditText texto){
+        texto.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length()>0) {
+                    if (estanLlenos()) {
+                        //Cargar operaciones
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+    private boolean estanLlenos(){
+        if(AngTan.getText().toString().isEmpty() ||
+                PI.getText().toString().isEmpty() ||
+                VP.getText().toString().isEmpty() ||
+                Le.getText().toString().isEmpty() ||
+                DC.getText().toString().isEmpty() ||
+                GC.getText().toString().isEmpty()){
+            //Alguno esta vacio
+            return false;
+        }else{
+            return true;
+        }
+    }
     private void createIntent(Class clase){
         /*method to go to other screens*/
         Intent intent = new Intent(this,clase);
@@ -57,5 +127,11 @@ public class SpiralValues extends AppCompatActivity {
         btnSave = (ImageButton) findViewById(R.id.SpiralSave);
         btnClear = (ImageButton) findViewById(R.id.SpiralClear);
         btnOperations = (Button) findViewById(R.id.btnOperacionesEspiral);
+        AngTan = (EditText) findViewById(R.id.deltaEspiral);
+        PI = (EditText) findViewById(R.id.PIEspiral);
+        VP = (EditText) findViewById(R.id.VPEspiral);
+        GC = (EditText) findViewById(R.id.GCEspiral);
+        Le = (EditText) findViewById(R.id.PIEspiral);
+        DC = (EditText) findViewById(R.id.DCEspiral);
     }
 }
