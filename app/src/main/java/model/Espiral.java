750080bcd620e89191ffd1ddd512432bda95294d
YorelyDamian <yorely1200@gmail.com>
2021-12-27 15:49:06 -0600

    Inicio de operaciones espiral

--- app/src/main/java/model/Espiral.java
+++ app/src/main/java/model/Espiral.java
@@ -2,4 +2,55 @@ package model;
 
 public class Espiral {
 
+    public int angTan;
+    public int pi;
+    public int vp;
+    public int gc;
+    public int le;
+    public int dc;
+
+    /*Creacion de get y set*/
+    /*get y set de AngTan*/
+    public int getAngTan() { return angTan; }
+    public void setAngTan(int angTan) { this.angTan = angTan; }
+    /*get y set de pi*/
+    public int getPi() { return pi; }
+    public void setPi(int pi) { this.pi = pi; }
+    /*get y set de Vp*/
+    public int getVp() { return vp; }
+    public void setVp(int vp) { this.vp = vp; }
+    /*get y set de gc*/
+    public int getGc() { return gc; }
+    public void setGc(int gc) { this.gc = gc; }
+    /*get y set de le*/
+    public int getLe() { return le; }
+    public void setLe(int le) { this.le = le; }
+    /*get y set de dc*/
+    public int getDc() { return dc; }
+    public void setDc(int dc) { this.dc = dc; }
+
+    public String operaciones(){
+        String resultado = "";
+        /*Operaciones Radio de curvatura (Rc)*/
+        /*Operaciones Parametro de la espiral (e)*/
+        /*Operaciones Deflexion de la curva circular (Ac)*/
+        /*Operaciones Coordenada de Gc de la curva en x (xc)*/
+        /*Operaciones Coordenada de Gc de la curva en y (yc)*/
+        /*Operaciones Coordenadas del Pc de la curva (P)*/
+        /*Operaciones Coordenadas del Pc de la curva (K)*/
+        /*Operaciones Tangente de la espiral (ste)*/
+        /*Operaciones Externa de la curva espiral (ec)*/
+        /*Operaciones Tagente larga (tl)*/
+        /*Operaciones Tangente corta (tc)*/
+        /*Operaciones Cuerda larga (Cle)*/
+        /*Operaciones Deflexion para Gc (pe)*/
+        /*Operaciones longitud de la curva circular (lc)*/
+        /*Operaciones Radio de curvatura*/
+
+        /*Operaciones Progresiva de (te)*/
+        /*Operaciones Progresiva  (ce)*/
+        /*Operaciones ce*/
+        /*Operaciones et*/
+        return resultado;
+    }
 }
