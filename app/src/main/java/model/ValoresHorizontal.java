073c739c66c514c3f132ca5ec3d538a436f74780
root <alexisluzpedraza123@gmail.com>
2021-12-28 11:21:06 -0600

    Creación de los GET y SET de curva horizontal

---
+++ app/src/main/java/model/ValoresHorizontal.java
@@ -0,0 +1,24 @@
+package model;
+
+public class ValoresHorizontal {
+    private String anguloTan;
+    private String puntoInter;
+    private String velocProy;
+    private String gradoCurva;
+
+    public String getAnguloTan() { return anguloTan; }
+
+    public void setAnguloTan(String anguloTan) { this.anguloTan = anguloTan; }
+
+    public String getPuntoInter() { return puntoInter; }
+
+    public void setPuntoInter(String puntoInter) { this.puntoInter = puntoInter; }
+
+    public String getVelocProy() { return velocProy; }
+
+    public void setVelocProy(String velocProy) { this.velocProy = velocProy; }
+
+    public String getGradoCurva() { return gradoCurva; }
+
+    public void setGradoCurva(String gradoCurva) { this.gradoCurva = gradoCurva; }
+}
