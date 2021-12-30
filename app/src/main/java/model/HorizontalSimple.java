153441eec320f7c4ac105389f4f0b5ed224dc4bc
Origaska <lucio.hernandez.noguez@gmail.com>
2021-12-29 23:28:28 -0600

    Calcular curva horizontal simple

--- app/src/main/java/model/HorizontalSimple.java
+++ app/src/main/java/model/HorizontalSimple.java
@@ -2,12 +2,32 @@ package model;
 
 public class HorizontalSimple {
     /*Creacion de variables para los datos de entrada*/
+
+    private String AT;
     private double anguloTan;
     private double puntoInter;
     private double velocProy;
+
+
     private double gradoCurva;
 
     private double RC;
+    private double ST;
+    private double LC;
+    private double CL;
+    private double M;
+    private double E;
+    private double PC;
+    private double PT;
+
+    /*GET DE AT PARA GMS*/
+    public String getAT() {
+        return AT;
+    }
+    /*SET DE AT PARA GMS*/
+    public void setAT(String AT) {
+        this.AT = AT;
+    }
 
     /*GET de Angulo de las tangentes*/
     public Double getAnguloTan() { return anguloTan; }
@@ -35,8 +55,16 @@ public class HorizontalSimple {
 
     public String calcularCurva(){
         String operaciones = "";
-
-
+        Grados obj = new Grados();
+        RC = 1145.92/gradoCurva;
+        anguloTan = obj.convertirADecimales(AT);
+        ST = RC*Math.tan(anguloTan/2);
+        LC = (20*anguloTan)/gradoCurva;
+        CL = 2 * RC * (Math.sin(anguloTan/2));
+        M = RC * (1-Math.cos(anguloTan/2));
+        E = RC * ((1/(Math.cos(anguloTan/2)))-1);
+        PC = puntoInter - ST;
+        PT = PC + LC;
 
         return operaciones;
     }
